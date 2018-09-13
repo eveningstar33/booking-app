8e6dab7f8353322f71b97c20b64e9cd906acbf30
Dragos Sarpe <contact@usdgadget.com>
2018-09-13 15:04:10 +0300

    Added configuration for changing the app behavior

--- src/main/java/com/dgs/bookingapp/controllers/ViewController.java
+++ src/main/java/com/dgs/bookingapp/controllers/ViewController.java
@@ -1,5 +1,7 @@
 package com.dgs.bookingapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,19 @@ import java.util.Date;
 @Controller
 public class ViewController {
 
+    private String appMode;
+
+    @Autowired
+    public ViewController(Environment environment) {
+        this.appMode = environment.getProperty("app-mode");
+    }
+
     @RequestMapping("/")
     public String index(Model model) {
 
         model.addAttribute("datetime", new Date());
         model.addAttribute("username", "Evening Star");
-        model.addAttribute("mode", "production");
+        model.addAttribute("mode", appMode);
 
         return "index";
     }
